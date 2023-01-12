f9e5f9c985eb9d067b3038b69ab446b4579b5eed
Ramy-Abousaif <ramydragolord@gmail.com>
2023-01-12 23:37:16 +0200

    Created Audio Manager

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,141 @@
+using FMOD.Studio;
+using FMODUnity;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    [Serializable]
+    public class sfxLib
+    {
+        public string Name;
+        public EventReference sfxPath;
+    }
+
+    [Header("Volume Sliders")]
+    public float masterVolume = 0.5f;
+    public float bgVolume = 0.5f;
+    public float sfxVolume = 0.5f;
+    public int testIndex = 2;
+
+    //FMOD Variables
+    Bus masterBus;
+    //Bus backgroundBus;
+    //Bus sfxBus;
+    EventInstance ambienceInstance;
+    EventInstance musicInstance;
+    EventInstance chargeInstance;
+    [Header("Events Selector")]
+    [Space(20)]
+    public EventReference ambienceEvent;
+    public EventReference musicEvent;
+    private static string sfxDir = "event:/SFX/";
+    public List<sfxLib> sfxObjectsList;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        masterBus = RuntimeManager.GetBus("Bus:/");
+        //backgroundBus = RuntimeManager.GetBus("Bus:/Master/Background");
+        //sfxBus = RuntimeManager.GetBus("Bus:/Master/SFX");
+        //StartAmbience();
+        //StartMusic();
+    }
+
+    private void Update()
+    {
+        masterBus.setVolume(this.masterVolume);
+        //backgroundBus.setVolume(this.bgVolume);
+        //sfxBus.setVolume(this.sfxVolume);
+        //musicInstance.setParameterByName("Scene", SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ChangeSFXVolume(float newSFXVolume)
+    {
+        this.sfxVolume = newSFXVolume;
+    }
+
+    public void ChangeBGVolume(float newBGVolume)
+    {
+        this.bgVolume = newBGVolume;
+    }
+
+    public void ChangeMasterVolume(float newMasterVolume)
+    {
+        this.masterVolume = newMasterVolume;
+    }
+
+    public void PlayOneShotWithParameters(string fmodEvent, Transform t, params (string name, float value)[] parameters)
+    {
+        EventInstance instance = RuntimeManager.CreateInstance(sfxDir + fmodEvent);
+
+        foreach (var (name, value) in parameters)
+        {
+            instance.setParameterByName(name, value);
+        }
+
+        if (t.GetComponent<Rigidbody>() != null)
+            RuntimeManager.AttachInstanceToGameObject(instance, t, t.GetComponent<Rigidbody>());
+
+        instance.set3DAttributes(t.position.To3DAttributes());
+        instance.start();
+        instance.release();
+    }
+
+    public void StartAmbience()
+    {
+        ambienceInstance = RuntimeManager.CreateInstance(ambienceEvent);
+        ambienceInstance.start();
+        ambienceInstance.release();
+    }
+
+    public void StartMusic()
+    {
+        musicInstance = RuntimeManager.CreateInstance(musicEvent);
+        musicInstance.start();
+        musicInstance.release();
+    }
+
+    public void ReleaseAmbience()
+    {
+        ambienceInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+    }
+
+    public void ReleaseMusic()
+    {
+        musicInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+    }
+
+    public void Resume()
+    {
+        masterBus.setPaused(false);
+    }
+
+    public void Pause()
+    {
+        masterBus.setPaused(true);
+    }
+
+    public void OnDestroy()
+    {
+        ReleaseAmbience();
+        ReleaseMusic();
+    }
+}
