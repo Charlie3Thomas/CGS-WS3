dd997d5c55913c700bfb6c44fd06ce1fd1ba5c18
Ramy-Abousaif <ramydragolord@gmail.com>
2023-01-16 03:39:26 +0000

    Can now generate a set of policies and they can be viewed on the cards

--- Assets/Scripts/Managers/PolicyManager.cs
+++ Assets/Scripts/Managers/PolicyManager.cs
@@ -9,7 +9,6 @@ public class PolicyManager : MonoBehaviour
     [System.Serializable]
     public class Policy
     {
-        public List<string> availableChoices = new List<string>();
         public string finalChoice;
         public int year;
         public int cost;
@@ -18,53 +17,43 @@ public class PolicyManager : MonoBehaviour
         public string resultOfEffect;
     }
 
-    private int numOfChoices = 3;
-    public string[] choices = { "Cut all trees", "Build a dam", "Increase taxes", "Kill john lennon", "Commit mass genocide", "Throw a party!",
+    private string[] choices = { "Cut all trees", "Build a dam", "Increase taxes", "Kill john lennon", "Commit mass genocide", "Throw a party!",
     "Lower taxes", "Throw waste into the ocean", "Build more roads", "Build more factories", "Build more houses" };
-    public List<Policy> policyList = new List<Policy>();
+    private List<Policy> policyList = new List<Policy>();
+    HashSet<string> finalChoices;
+    private int numOfPolicies = 7;
 
     private void Start()
     {
-        for (int i = 0; i < UIController.Instance.pCardText.Length; i++)
-        {
-            //NewRandomPolicy(i);
-        }
+        NewPolicySet();
     }
 
-    void NewRandomPolicy(int cardNumber)
+    void NewPolicySet()
     {
-        Policy pol = new Policy();
-
-        for (int i = 0; i < numOfChoices; i++)
+        int policyCount = 0;
+        HashSet<string> finalChoices = new HashSet<string>();
+        while (policyCount < numOfPolicies)
         {
-            string currentChoice = choices[Random.Range(0, choices.Length)];
-            while (!pol.availableChoices.Contains(currentChoice))
+            Policy pol = new Policy();
+            pol.finalChoice = choices[Random.Range(0, choices.Length)];
+            if (finalChoices.Contains(pol.finalChoice))
             {
-                pol.availableChoices.Add(currentChoice);
-                numOfChoices--;
+                continue;
             }
-            numOfChoices++;
+            else
+            {
+                finalChoices.Add(pol.finalChoice);
+                pol.year = YearData._INSTANCE.current_year;
+                pol.cost = Random.Range(-10000, 10000);
+                pol.publicFavour = Random.Range(-10000, 10000);
+                pol.awareness = Random.Range(-10000, 10000);
+                policyList.Add(pol);
+                policyList.Sort(SortByYear);
+                UIController.Instance.pCardText[policyCount].text = pol.finalChoice + "\nCost: " + pol.cost +
+                "\nPublic Favour: " + pol.publicFavour + "\nAwareness: " + pol.awareness;
+                policyCount++;
+            }
         }
-
-        pol.finalChoice = pol.availableChoices[Random.Range(0, pol.availableChoices.Count)];
-        pol.year = YearData._INSTANCE.current_year;
-        pol.cost = Random.Range(-10000, 10000);
-        pol.publicFavour = Random.Range(-10000, 10000);
-        pol.awareness = Random.Range(-10000, 10000);
-
-        UIController.Instance.pCardText[cardNumber].text = pol.finalChoice + "\nCost: " + pol.cost +
-            "\nPublic Favour: " + pol.publicFavour + "\nAwareness: " + pol.awareness;
-        /*
-        pol.resultOfEffect = "A policy of '" + pol.finalChoice + "' has been taken " 
-            + ((pol.cost > 0) ? "which has cost you $" : "which has made you gain $") + Mathf.Abs(pol.cost) + ", "
-            + ((pol.publicFavour > 0) ? "which has also gained the public's favour " : "which has also reduced the public's favour") + " by " + Mathf.Abs(pol.publicFavour) + " points and "
-            + ((pol.awareness > 0) ? "has increased your awareness" : "has reduced your awareness") + " by " + Mathf.Abs(pol.awareness) + " points.";
-
-        Debug.Log(pol.resultOfEffect);
-        */
-        policyList.Add(pol);
-        var pol1 = pol;
-        policyList.Sort(SortByYear);
     }
 
     private static int SortByYear(Policy pol1, Policy pol2)
