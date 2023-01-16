160903def1f8321bdb009341563480b8854bd5f9
Ramy-Abousaif <ramydragolord@gmail.com>
2023-01-16 23:38:16 +0000

    Added anim events for notepad to play sound effects

---
+++ Assets/Scripts/UI/Notepad.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notepad : MonoBehaviour
+{
+    public void PlayShowSound()
+    {
+        Debug.Log("Play Show Sound Effect");
+    }
+
+    public void PlayHideSound()
+    {
+        Debug.Log("Play Hide Sound Effect");
+    }
+}
