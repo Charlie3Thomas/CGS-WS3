7bacf8983619141a073ce42e0d25ad7a1fb5de74
Ramy-Abousaif <ramydragolord@gmail.com>
2023-01-31 22:28:00 +0000

    New UI + appropriate changes

--- Assets/Scripts/Computer/Button.cs
+++ Assets/Scripts/Computer/Button.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public enum buttonType
 {
     RESET,
-    PAN_UP,
-    PAN_DOWN,
+    CONFIRM_ALLOCATION,
     CONFIRM_YEAR,
     JOURNAL_NOTEPAD
 }
@@ -17,31 +16,26 @@ public class Button : MonoBehaviour
 
     public void Press()
     {
-        switch(type)
+        switch (type)
         {
             case buttonType.RESET:
                 // Reset
                 Debug.Log("Reset");
                 break;
-            case buttonType.PAN_DOWN:
-                // Pan down to journal
-                Debug.Log("Pan Down");
-                AudioPlayback.PlayOneShot(AudioManager.Instance.uiEvents.buttonPressLEvent, null);
-                break;
-            case buttonType.PAN_UP:
-                // Pan up to tech tree screen
-                Debug.Log("Pan Up");
-                AudioPlayback.PlayOneShot(AudioManager.Instance.uiEvents.buttonPressLEvent, null);
+            case buttonType.CONFIRM_ALLOCATION:
+                // Confirm allocation
+                Debug.Log("Confirm allocation");
                 break;
             case buttonType.CONFIRM_YEAR:
                 // Confirm year
                 Debug.Log("Confirm year");
-                AudioPlayback.PlayOneShot(AudioManager.Instance.uiEvents.buttonPressREvent, null);
                 break;
             case buttonType.JOURNAL_NOTEPAD:
                 // Change between journal and notepad
                 Debug.Log("Change screen between notepad and journal");
                 break;
         }
+
+        AudioPlayback.PlayOneShot(AudioManager.Instance.uiEvents.buttonPressLEvent, null);
     }
 }
