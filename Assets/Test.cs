8f9661e397d185d8e914cfd8b99f260b95653b6a
Charlie <charlie3.thomas@live.uwe.ac.uk>
2023-02-08 19:04:45 +0000

    [CGS_WS3] Pre merge push

---
+++ Assets/Test.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField] private int money;
+    [SerializeField] private int science;
+    [SerializeField] private int food;
+}
