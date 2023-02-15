7bd9221c4c04cf1b617c1feaea77f15aca3a7927
Ramy-Abousaif <ramydragolord@gmail.com>
2023-02-15 19:11:39 +0000

    Nodes now glow when unlocked

--- Assets/Scripts/Computer/TechNode.cs
+++ Assets/Scripts/Computer/TechNode.cs
@@ -9,15 +9,21 @@ public class TechNode : MonoBehaviour
     public bool unlocked;
     public TechNode[] requiredNodes;
     private List<LineRenderer> lineRenderers;
-    public Material mat;
-    private float lineWidth = 5f;
+    public Material lineMat;
+    private float lineWidth = 1.5f;
     public AllocType effectType;
     public float effect;
+
+    private Material mat;
+    private Color faded = new Color(0.5f, 0.5f, 0.5f) * 1;
+    public Color Lit;
 
     private void Start()
     {
         tree = GameObject.FindGameObjectWithTag("TechTree").GetComponent<TechTree>();
         lineRenderers = new List<LineRenderer>();
+        mat = GetComponent<Renderer>().material;
+        mat.SetVector("_Color", faded);
 
         if (requiredNodes.Length <= 0)
             return;
@@ -27,16 +33,24 @@ public class TechNode : MonoBehaviour
             TechNode requiredNode = requiredNodes[i];
             GameObject lineObj = new GameObject("lineObj");
             lineObj.transform.SetParent(requiredNode.transform);
+            lineObj.transform.localEulerAngles = new Vector3(90, 0, 0);
             lineObj.layer = LayerMask.NameToLayer("TechTree");
             LineRenderer lineRenderer = lineObj.AddComponent<LineRenderer>();
             if(lineRenderer != null)
             {
-                lineRenderer.material = mat;
+                lineRenderer.material = lineMat;
+                lineRenderer.alignment = LineAlignment.TransformZ;
+                lineRenderer.sortingOrder = -1;
                 lineRenderer.positionCount = 2;
                 lineRenderer.startWidth = lineWidth;
                 lineRenderer.endWidth = lineWidth;
-                lineRenderer.SetPosition(0, requiredNode.transform.position);
-                lineRenderer.SetPosition(1, transform.position);
+
+                lineRenderer.SetPosition(0, new Vector3(requiredNode.transform.position.x + (transform.localScale.x / 2), 
+                    requiredNode.transform.position.y + 2.5f, requiredNode.transform.position.z));
+
+                lineRenderer.SetPosition(1, new Vector3(transform.position.x - (transform.localScale.x / 2), 
+                    transform.position.y + 2.5f, transform.position.z));
+
                 lineRenderers.Add(lineRenderer);
             }
         }
@@ -46,7 +60,7 @@ public class TechNode : MonoBehaviour
     {
         if (tree == null)
         {
-            Debug.Log("Parent does not contain the tech tree script, can not unlock without it");
+            Debug.Log("Tech tree not found, can't upgrade without a tech tree gameobject with respective tag and component");
             return;
         }
 
@@ -70,6 +84,7 @@ public class TechNode : MonoBehaviour
             if (tree.sciencePoints >= pointsRequired)
             {
                 unlocked = true;
+                mat.SetVector("_Color", Lit * 8);
                 tree.sciencePoints -= pointsRequired;
             }
             else
