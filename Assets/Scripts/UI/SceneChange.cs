e5b427197ccb74f31efb14c81f8a5b8185752118
deadheart123 <shreyas2799@gmail.com>
2023-04-02 01:59:58 +0100

    Linked Audio Settings to UI, Added One-shots for few buttons
    Linked Audio Settings to UI, Added One-shots for few buttons

--- Assets/Scripts/UI/SceneChange.cs
+++ Assets/Scripts/UI/SceneChange.cs
@@ -5,8 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
-  
 
+    // This script consists of button press events for changing scenes, enabling disabling canvas objects and to trigger Audio events based on it
+
+    // All button functionality to be added to this script and removed from the Inspector, once all UI elements are finalized.
+
+    #region MAIN MENU BUTTONS AND AUDIO EVENT TRIGGERS
     public void OnSelectStartGame()
     {
         SceneManager.LoadScene(1);
@@ -28,5 +32,20 @@ public class SceneChange : MonoBehaviour
         Application.Quit();
     }
 
+    public void OnSelectBack() 
+    {
+        AudioPlayback.PlayOneShot(MenuAudioManager.instance.mainMenuRefs.menuButtonSelectEvent, null);
+    }
 
+    public void OnSelectAudioSetttings()
+    {
+        AudioPlayback.PlayOneShot(MenuAudioManager.instance.mainMenuRefs.menuButtonSelectEvent, null);
+    }
+
+    public void OnSelectExitAudioSettings()
+    {
+        AudioPlayback.PlayOneShot(MenuAudioManager.instance.mainMenuRefs.menuButtonSelectEvent, null);
+    }
 }
+
+#endregion
