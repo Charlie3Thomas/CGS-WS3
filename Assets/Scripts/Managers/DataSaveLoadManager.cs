c54f9dbf9135118d00ce514c33f5d422c4bc899d
hirenkacha <hirenkacha87@gmail.com>
2023-04-13 18:09:16 +0100

    [DATA_SAVE] Replaced JsonUtility with NewtonSoft Json.NET

--- Assets/Scripts/Managers/DataSaveLoadManager.cs
+++ Assets/Scripts/Managers/DataSaveLoadManager.cs
@@ -6,6 +6,8 @@ using Unity.Services.Authentication;
 using System.Threading.Tasks;
 using Unity.Services.Core;
 using Unity.Services.CloudSave;
+using Newtonsoft.Json;
+using System.IO;
 
 [Serializable]
 public class ResourceData
@@ -23,6 +25,7 @@ public class DisasterData
 {
     public int numOfDisasters;
     public List<Disaster> disasterList;
+    public Dictionary<int, DisasterEnum.type> disaster_timeline;
 }
 [Serializable]
 public class PolicyData
@@ -33,11 +36,28 @@ public class PolicyData
     public HashSet<string> finalChoices;
 }
 [Serializable]
+public class YearSaveData
+{
+    public Dictionary<int, bool> changed_years;
+    public int earliest_year;
+    public int latest_year;
+    public int current_year;
+}
+[Serializable]
+public class FactionData
+{
+    public Dictionary<int, Dictionary<FactionEnum.type, int>> number_data;
+    public Dictionary<int, Dictionary<FactionEnum.type, int>> happiness_data;
+    public Dictionary<int, Dictionary<FactionEnum.type, int>> budget_data;
+}
+[Serializable]
 public class PlayerData
 {
     public ResourceData resources;
     public DisasterData disasters;
     public PolicyData policies;
+    public YearSaveData yearData;
+    public FactionData factionData;
 }
 
 public class DataSaveLoadManager : MonoBehaviour
@@ -45,16 +65,20 @@ public class DataSaveLoadManager : MonoBehaviour
     private ResourceManager resourceManager;
     private DisasterManager disasterManager;
     private PolicyManager policyManager;
+    private YearData yearData;
+    private DisasterTimelineData disasterTimelineData;
+    private FactionNumberData factionNumberData;
+    private FactionHappinessData factionHappinessData;
+    private FactionBudgetData factionBudgetData;
 
-    private const string resourceKey = "RESOURCE";
-    private const string disasterKey = "DISASTER";
-    private const string policyKey = "POLICY";
     private const string playerDataKey = "PLAYER_DATA";
 
     private ResourceData resourceData;
     private DisasterData disasterData;
     private PolicyData policyData;
     private PlayerData playerData;
+    private YearSaveData yearSaveData;
+    private FactionData factionData;
 
     private static DataSaveLoadManager _instance;
     public static DataSaveLoadManager Instance
@@ -73,6 +97,11 @@ public class DataSaveLoadManager : MonoBehaviour
         resourceManager = ResourceManager.instance;
         disasterManager = DisasterManager.instance;
         policyManager = PolicyManager.instance;
+        yearData = YearData._INSTANCE;
+        disasterTimelineData = DisasterTimelineData._INSTANCE;
+        factionNumberData = FactionNumberData._INSTANCE;
+        factionHappinessData = FactionHappinessData._INSTANCE;
+        factionBudgetData = FactionBudgetData._INSTANCE;
     }
 
     void Update()
@@ -88,15 +117,15 @@ public class DataSaveLoadManager : MonoBehaviour
         saveResource();
         saveDisaster();
         savePolicy();
+        saveYearData();
+        saveFactionData();
         savePlayerData();
     }
 
     public void Load()
     {
         loadPlayerData();
-        loadResources();
-        loadDisaster();
-        loadPolicy();
+        
     }
 
     private void saveResource()
@@ -110,8 +139,8 @@ public class DataSaveLoadManager : MonoBehaviour
         resourceData.currency = resourceManager.current_currency;
         resourceData.turnList = resourceManager.turnList;
 
-        string s = JsonUtility.ToJson(resourceData);
-        PlayerPrefs.SetString(resourceKey, s);
+        //string s = JsonConvert.SerializeObject(resourceData);
+        //PlayerPrefs.SetString(resourceKey, s);
 
         Debug.Log("Resources Saved");
     }
@@ -134,6 +163,7 @@ public class DataSaveLoadManager : MonoBehaviour
         disasterData = new DisasterData();
         disasterData.numOfDisasters = disasterManager.numOfDisasters;
         disasterData.disasterList = disasterManager.disasterList;
+        disasterData.disaster_timeline = disasterTimelineData.disaster_timeline;
         //string s = JsonUtility.ToJson(disasterData);
         //PlayerPrefs.SetString(disasterKey, s);
     }
@@ -142,6 +172,7 @@ public class DataSaveLoadManager : MonoBehaviour
         disasterData = playerData.disasters;
         disasterManager.numOfDisasters = disasterData.numOfDisasters;
         disasterManager.disasterList = disasterData.disasterList;
+        disasterTimelineData.disaster_timeline = disasterData.disaster_timeline;
         Debug.Log("Disaster loaded");
     }
     private void savePolicy()
@@ -163,24 +194,69 @@ public class DataSaveLoadManager : MonoBehaviour
         Debug.Log("Policy loaded");
     }
 
+    private void saveYearData()
+    {
+        yearSaveData = new YearSaveData();
+        yearSaveData.changed_years = yearData.changed_years;
+        yearSaveData.current_year = yearData.current_year;
+        yearSaveData.earliest_year = yearData.earliest_year;
+        yearSaveData.latest_year = yearData.latest_year;
+    }
+
+    private void loadYearData()
+    {
+        yearSaveData = playerData.yearData;
+        yearData.current_year = yearSaveData.current_year;
+        yearData.latest_year = yearSaveData.latest_year;
+        yearData.earliest_year = yearSaveData.earliest_year;
+        yearData.changed_years = yearSaveData.changed_years;
+    }
+
+    private void saveFactionData()
+    {
+        factionData = new FactionData();
+        factionData.number_data = factionNumberData.number_data;
+        factionData.happiness_data = factionHappinessData.happiness_data;
+        factionData.budget_data = factionBudgetData.budget_data;
+    }
+
+    private void loadFactionData()
+    {
+        factionData = playerData.factionData;
+        factionNumberData.number_data = factionData.number_data;
+        factionHappinessData.happiness_data = factionData.happiness_data;
+        factionBudgetData.budget_data = factionData.budget_data;
+    }
+
     private async void savePlayerData()
     {
         playerData = new PlayerData();
         playerData.resources = resourceData;
         playerData.disasters = disasterData;
         playerData.policies = policyData;
-        string s = JsonUtility.ToJson(playerData);
+        playerData.yearData = yearSaveData;
+        playerData.factionData = factionData;
+        string s = JsonConvert.SerializeObject(playerData);
         PlayerPrefs.SetString(playerDataKey, s);
+        Debug.Log($"SAVING DATA : {s}");
+        writeToFile(s);
         await ForceSaveSingleData(playerDataKey, s);
     }
 
     private async void loadPlayerData()
     {
         playerData = await RetrieveSpecificData<PlayerData>(playerDataKey);
+        //Debug.Log("DATA RECEIVED :: " + playerData.resources.total_population);
+        
         resourceData = playerData.resources;
         disasterData = playerData.disasters;
         policyData = playerData.policies;
-        Debug.Log("DATA RECEIVED :: " + resourceData.total_population);
+
+        loadResources();
+        loadDisaster();
+        loadPolicy();
+        loadYearData();
+        loadFactionData();
     }
 
     private async Task ForceSaveSingleData(string key, string value)
@@ -214,7 +290,7 @@ public class DataSaveLoadManager : MonoBehaviour
 
             if (results.TryGetValue(key, out string value))
             {
-                return JsonUtility.FromJson<T>(value);
+                return JsonConvert.DeserializeObject<T>(value);
             }
             else
             {
@@ -235,5 +311,18 @@ public class DataSaveLoadManager : MonoBehaviour
         }
 
         return default;
+    }
+    private void writeToFile(string str)
+    {
+        string path = Application.dataPath + "/StreamingAssets/";
+        StreamWriter sw = new StreamWriter(path + "saveData.txt");
+        try
+        {
+            sw.Write(str);
+        }
+        catch (FileNotFoundException e)
+        {
+            Debug.Log("Error opening or writing file saveData.txt" + e.Message);
+        }
     }
 }
