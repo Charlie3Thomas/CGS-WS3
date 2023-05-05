be147b93840e9b88da93262f6aabeef8a6aca623
deadheart123 <shreyas2799@gmail.com>
2023-05-05 18:20:35 +0100

    Updated Scorecard UI to display points
    Updated Scorecard UI to display points

--- Assets/Scripts/UI/Scorecard/ScorecardUI.cs
+++ Assets/Scripts/UI/Scorecard/ScorecardUI.cs
@@ -2,15 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using TMPro;
+using UnityEngine.UI;
 
 public class ScorecardUI : MonoBehaviour
 {
-
+    // Animation Variables
     public float fadetime = 5f;
     public CanvasGroup canvasGroup; 
     public RectTransform rectTransform;
     public List<GameObject> items = new List<GameObject>();
 
+
+    // Point UI and Scorecard variables
+    public List<TextMeshProUGUI> pointsUI = new List<TextMeshProUGUI>();
+    // incremental counters for every field
+    private float counter1 = 0.0f;
+    private float counter2 = 0.0f;
+    private float counter3 = 0.0f;
+    private float counter4 = 0.0f;
+
+    private float awarenessPoints=1000.0f;
+    private float disasterPoints=25000.0f;
+    private float nodePoints=20000f;
+    private float yearPoints=22000f;
+
+    private float incrementRate1 =10f;
+    private float incrementRate2 = 10f;
+    private float incrementRate3 = 10f;
+    private float incrementRate4 = 10f;
+
+
+
+    private void Start()
+    {
+        incrementRate1 = awarenessPoints / (50 * 4);
+        incrementRate2 = disasterPoints / (50 * 4);
+        incrementRate3 = nodePoints / (50 * 4);
+        incrementRate4 = yearPoints / (50 * 4);
+    }
+
+
+    private void FixedUpdate()
+    {
+
+        IncrementPoints();
+    }
 
     public void FadeInAnimation()
     {
@@ -37,6 +74,43 @@ public class ScorecardUI : MonoBehaviour
 
         }
     }
+
+    private void IncrementPoints()
+    {
+        
+
+        if (counter1 < awarenessPoints)
+        {
+            counter1 += incrementRate1;
+            pointsUI[0].text = counter1.ToString();
+        }
+       
+
+        if (counter2 < disasterPoints)
+        {
+            counter2 += incrementRate2;
+            pointsUI[1].text = counter2.ToString();
+        }
+        
+
+        if (counter3 < nodePoints)
+        {
+            counter3 += incrementRate3;
+            pointsUI[2].text = counter3.ToString();
+        }
+        
+
+        if (counter4 < yearPoints)
+        {
+            counter4 += incrementRate4;
+            pointsUI[3].text = counter4.ToString();
+        }
+       
+
+    }
+
+
+
 
 
 }
