f6c91ee0dcb78bedcb84e597005808b51ea539a1
samsquanchone <105938519+samsquanchone@users.noreply.github.com>
2023-05-10 20:01:10 +0100

    Tutorial audio added

---
+++ Assets/TutorialAudio.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMOD.Studio;
+using FMODUnity;
+
+public class TutorialAudio : MonoBehaviour
+{
+    public EventReference tutorialSFX;
+   
+}
