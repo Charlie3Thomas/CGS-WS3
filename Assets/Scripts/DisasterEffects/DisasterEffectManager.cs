c7729e0c35c9c70ab057831708916771ae4c1f75
Ramy-Abousaif <ramydragolord@gmail.com>
2023-05-11 04:58:16 +0100

    More disasters

--- Assets/Scripts/DisasterEffects/DisasterEffectManager.cs
+++ Assets/Scripts/DisasterEffects/DisasterEffectManager.cs
@@ -3,17 +3,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using CT;
 
 public class DisasterEffectManager : MonoBehaviour
 {
+    public static DisasterEffectManager instance;
+
+    public GameObject currentEffect;
     [SerializeField]
     private CinemachineImpulseSource impulseSrouce;
     [SerializeField]
     private CinemachineVirtualCamera mapVCam;
+
+    // Tornado
     [SerializeField]
     private GameObject tornado;
+
+    // Tsunami
     [SerializeField]
     private GameObject tsunamiWave;
+    [SerializeField]
+    private Transform tsunamiTransform;
+
+    // Volcano
+    [SerializeField]
+    private ParticleSystem volcano;
+
+    public float testIntensity = 1f;
+    public float duration = 10f;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(this.gameObject);
+            Debug.LogError("Multiple DisasterEffectManager Instances!");
+        }
+        else
+            instance = this;
+    }
 
     void Start()
     {
@@ -21,37 +49,82 @@ public class DisasterEffectManager : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.D))
-        {
-        }
     }
 
-    private void ShowDisasterEffect(Disaster disaster)
+    public void ShowDisasterEffect(CTDisasters type, float intensity)
     {
-        float intensity;
-        switch (disaster.type)
+        if(currentEffect != null)
+        {
+            Destroy(currentEffect);
+            CinemachineImpulseManager.Instance.Clear();
+        }    
+
+        switch (type)
         {
             case CTDisasters.Earthquake:
-                intensity = 2f * disaster.intensity;
-                Shake(intensity);
+                StartCoroutine(EarthquakeEffect(intensity));
                 break;
             case CTDisasters.Tsunami:
-                intensity = 1.2f * disaster.intensity;
-                Shake(intensity);
+                StartCoroutine(TsunamiEffect(intensity));
                 break;
             case CTDisasters.Volcano:
-                intensity = 1.5f * disaster.intensity;
+                StartCoroutine(VolcanoEffect(intensity));
                 Shake(intensity);
                 break;
             case CTDisasters.Tornado:
-                intensity = 1.2f * disaster.intensity;
-                Shake(intensity);
+                StartCoroutine(TornadoEffect(intensity));
                 break;
             case CTDisasters.None:
                 break;
             default:
                 break;
         }
+    }
+
+    private IEnumerator TsunamiEffect(float _intensity)
+    {
+        float intensity = 1.3f * _intensity;
+        Shake(intensity);
+        GameObject tGO = Instantiate(tsunamiWave, tsunamiTransform.position, tsunamiTransform.rotation);
+        currentEffect = tGO;
+        tGO = currentEffect;
+        float size = RAUtility.Remap(_intensity, 1, 10, 0.5f, 1.0f);
+        tGO.transform.GetChild(0).localScale = new Vector3(tGO.transform.GetChild(0).localScale.x * size,
+            tGO.transform.GetChild(0).localScale.y * size, tGO.transform.GetChild(0).localScale.z * size);
+        // Affect buildings (Sweep along with water)
+        yield return new WaitForSeconds(duration);
+        currentEffect = null;
+        Destroy(tGO);
+        yield return null;
+    }
+
+    private IEnumerator EarthquakeEffect(float _intensity)
+    {
+        float intensity = 2f * _intensity;
+        Shake(intensity);
+        // Affect buildings (Sink down ground)
+        yield return null;
+    }
+
+    private IEnumerator VolcanoEffect(float _intensity)
+    {
+        float intensity = 1.5f * _intensity;
+        Shake(intensity);
+        // Play effect
+        volcano.Play();
+
+        // Affect buildings (Blow them up)
+        yield return null;
+    }
+
+    private IEnumerator TornadoEffect(float _intensity)
+    {
+        float intensity = 1.2f * _intensity;
+        Shake(intensity);
+        // Play effect and destroy if necessary
+
+        // Affect buildings (Sweep them up the screen)
+        yield return null;
     }
 
     private void Shake(float intensity)
--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -399,6 +399,10 @@ namespace CT
 
             //GetChangesAtTurn();
             turn_data = GetYearData(_requested_turn);
+
+            if (DisasterEffectManager.instance != null && disaster_timeline[current_turn]?.disaster != null)
+                DisasterEffectManager.instance.ShowDisasterEffect(disaster_timeline[current_turn].disaster, disaster_timeline[current_turn].intensity);
+
             AudioManager.Instance.StartDisasterAudio(CheckDisasterInTurn(), GetDisasterIntensityAtTurn(current_turn));
             PolicyManager.instance.LoadPoliciesAtCurrentScope(current_turn);
             empty_turn_resource_expenditure = new Vector3(0, 0, 0);
